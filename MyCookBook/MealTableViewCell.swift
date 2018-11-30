a79b4e18dfb59ee0081e19db530bec95472ab0f6
Unknown <antoine.caffier@epitech.eu>
2018-11-30 20:37:37 +0800

    View

---
+++ MyCookBook/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  MyCookBook
+//
+//  Created by Antoine on 30/11/2018.
+//  Copyright © 2018 Antoine. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyCookBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCookBook
+//
+//  Created by Antoine on 30/11/2018.
+//  Copyright © 2018 Antoine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
